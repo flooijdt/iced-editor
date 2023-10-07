6c4a549f5008850970d65a5b36599ad01d580172
flooijdt <flooijdt@posteo.de>
2023-10-06 21:29:16 -0300

    default commit.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use iced::{widget::text, Element, Sandbox};
+
 fn main() {
     println!("Hello, world!");
+}
+
+struct Editor;
+
+#[derive(Debug)]
+enum Message {}
+
+impl Sandbox for Editor {
+    // a message is basically any event that alters the app's state. (eg: clicking something.).
+    type Message = Message;
+
+    fn new() -> Self {
+        Self
+    }
+
+    fn title(&self) -> String {
+        String::from("A cool editor.")
+    }
+
+    fn update(&mut self, message: Self::Message) {
+        match message {}
+    }
+
+    fn view(&self) -> Element<'_, Message> {
+        text("Hello iced!").into()
+    }
 }
